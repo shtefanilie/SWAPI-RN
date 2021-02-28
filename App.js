8750670fd6d2e9796e37f7e899b496936c67a202
Stefan Ilie <stefanionut92@gmail.com>
2021-02-28 21:58:15 +0000

    some work

--- App.js
+++ App.js
@@ -10,11 +10,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import RootNavigator from './src/views';
 import { store } from './src/store';
+import { ThemeProvider } from '@react-navigation/native';
+import { lightTheme } from './src/components/theme';
 
 const App: () => React$Node = () => {
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <ThemeProvider theme={lightTheme}>
+        <RootNavigator />
+      </ThemeProvider>
     </Provider>
   );
 };
---
+++ src/components/theme.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  ContentContainer: {
+    backgroundColor: "white",
+    paddingLeft: 2,
+    paddingRight: 2,
+  }
+}
+
+export const darkTheme = {
+  ContentContainer: {
+    backgroundColor: "#222831",
+    paddingLeft: 2,
+    paddingRight: 2,
+  }
+}
